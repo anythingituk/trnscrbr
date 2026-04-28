99b1f057c6099f8deb08877c798cf78942d43252
Jonathan Burrows <jonathan@anythingit.net>
2026-04-28 15:31:09 +0100

    Read app version from assembly metadata

--- src/Trnscrbr/AppInfo.cs
+++ src/Trnscrbr/AppInfo.cs
@@ -1,7 +1,13 @@
+using System.Reflection;
+
 namespace Trnscrbr;
 
 public static class AppInfo
 {
-    public const string Version = "0.1.0";
     public const string ReleasesUrl = "https://github.com/anythingituk/trnscrbr/releases";
+
+    public static string Version { get; } =
+        typeof(AppInfo).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+        ?? typeof(AppInfo).Assembly.GetName().Version?.ToString()
+        ?? "unknown";
 }
